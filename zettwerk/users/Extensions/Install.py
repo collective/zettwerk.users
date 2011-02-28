ead710a837bb6cba42b375ae70db1004f112a945
chrigl <chrigl@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-02-28 14:47:56 +0000

    - Changed name of controlpanel link - Controlpanel icons. plone3 backward compatibility

--- zettwerk/users/Extensions/Install.py
+++ zettwerk/users/Extensions/Install.py
@@ -1,4 +1,9 @@
 from Products.CMFCore.utils import getToolByName
+try:
+    import plone.app.upgrade
+    PLONE_VERSION = 4
+except ImportError:
+    PLONE_VERSION = 3
 
 
 def uninstall(portal):
@@ -6,6 +11,14 @@ def uninstall(portal):
     setup_tool.runAllImportStepsFromProfile('profile-zettwerk.users:uninstall')
 
     cp_tool = getToolByName(portal, 'portal_controlpanel')
-    cp_tool.unregisterConfiglet('zettwerkusers')
+    cp_tool.unregisterConfiglet('zettwerkuserlist')
+
+    if PLONE_VERSION == 3:
+        ai_tool = getToolByName(portal, 'portal_actionicons')
+        try:
+            ai_tool.removeActionIcon('controlpanel', 'zettwerkuserlist')
+        except KeyError:
+            # was not there, so nothing removed
+            pass
 
     return "Ran all uninstall steps for zettwerk.users"
